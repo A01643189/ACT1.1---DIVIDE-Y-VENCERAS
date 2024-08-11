78beef3455a4815ca20e7040804b8f1c195559ca
A01643189 <a01643189@tec.mx>
2024-08-10 22:13:21 -0600

    change array to vector

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
+#include <vector>
 
 int main() {
     std::cout << "Merge Sort" << std::endl;
-    int list[] = {12, 11, 13, 5, 6, 7};
-    int n = sizeof(list) / sizeof(list[0]);
+    std::vector<int> list = {12, 11, 13, 5, 6, 7};
+    int length = list.size();
 
     std::cout << "Unordered list" << std::endl;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < length; i++) {
         std::cout << list[i] << " ";
     }
 
     std::cout << "Ordered list" << std::endl;
 
-     for (int i = 0; i < n; i++) {
+    for (int i = 0; i < length; i++) {
         std::cout << list[i] << " ";
     }
 
